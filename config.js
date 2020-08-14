f0fce16213676e1979457f608a4ee19e0ac4b4a7
traaitt | TRRXITTE inc <traaitt@icloud.com>
2020-08-14 11:16:48 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,20 @@
+var api = 'https://europe-west.traaitt.com';
+	var donationAddress = "cashKdCEq5U7W2QDS5ffMETf1smoKLBm3C1GMYvPdEVUU9LKr1uDFrLNPkmfKgzcTcB4ASsypziCeVQ5iqkzC7fZ1pDDLSxgZF";
+	var blockTargetInterval = 144; // enter the block interval in seconds
+	var coinUnits = 100000000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
+	var totalSupply =  8874400000000000; // enter the total supply in atomic units
+	var symbol = 'TCH'; // enter the coin's ticker
+	var refreshDelay = 30000;
+	
+	// pools stats by MainCoins
+	var networkStat = {
+	 "TCH": [
+		[""]
+	 ]
+	};
+	
+	var networkStat2 = {
+	    "TCH": [
+		[""]
+	 ]
+	};
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+        'api' => 'https://europe-west.traaitt.com',
+        'blockTargetInterval' => 144,
+        'coinUnits' => 100
+);
