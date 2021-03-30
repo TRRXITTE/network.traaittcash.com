a7f1c62cd6ecf8a1989e85ae3420f218e5ba46ed
traaitt | TRRXITTE inc <traaitt@icloud.com>
2021-03-30 07:43:15 +0200

    XTCASH

--- config.js
+++ config.js
@@ -3,18 +3,18 @@ var api = 'https://europe-west.traaitt.com';
 	var blockTargetInterval = 144; // enter the block interval in seconds
 	var coinUnits = 100000000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
 	var totalSupply =  8874400000000000; // enter the total supply in atomic units
-	var symbol = 'TCH'; // enter the coin's ticker
+	var symbol = 'XTCASH'; // enter the coin's ticker
 	var refreshDelay = 30000;
 	
 	// pools stats by MainCoins
 	var networkStat = {
-	 "TCH": [
+	 "XTCASH": [
 		[""]
 	 ]
 	};
 	
 	var networkStat2 = {
-	    "TCH": [
+	    "XTCASH": [
 		[""]
 	 ]
 	};
